1842768cdcd99e1e7c4da8145aa8f1e26f301b0f
Mihai <flashjpr@gmail.com>
2016-03-20 05:05:29 +0000

    Encryption using custom base64 algorithm

---
+++ base64.h
@@ -0,0 +1,63 @@
+#ifndef BASE64_H
+#define BASE64_H
+
+#include <vector>
+#include <string>
+
+namespace Base64 {
+	std::string base64_encode (const std::string &);
+
+	const std::string &SALT1 = "LM::TB::BB";
+	const std::string &SALT2 = "_:/_77";
+	const std::string &SALT3 = "line=wowC++";
+
+	std::string EncryptB64 (std::string s) {
+		s = SALT1 + s + SALT2 +  SALT3;
+		s = base64_encode (s);
+		s.insert(7, SALT3);
+		s += SALT1;
+		s = base64_encode (s);
+		s = SALT2 + SALT3 + SALT1;
+		s = base64_encode (s);
+		s.insert(1, "L");
+		s.insert(7, "m");
+		return s;
+	}
+
+	const std::string &BASE64_CODES = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+	const std::string base64_encode (const std::string &s) {
+		std::string output;
+		int val = 0;
+		int bits = -6;
+		const unsigned int b63 = 0x3F; 
+
+		for (const auto &c : s) {
+			// Shift left by 8 bits
+			val = (val << 8) + c;
+			// Extract information from original strings in octets
+			bits += 8;
+
+			while (bits >= 0) {
+				output.push_back(BASE64_CODES[(val >> bits) & b63]);
+				bits -= 6;
+			}
+		} // for 
+
+		if (bits > -6) {
+			output.push_back(BASE64_CODES[((val << 8) >> (bits + 8)) & b63]);
+		}
+
+		while (output.size() % 4) {
+			output.push_back('=');
+		}
+
+		return output;
+	}
+
+	std::string DecryptB64 (std::string s) {
+
+	}
+}
+
+#endif // BASE_64
